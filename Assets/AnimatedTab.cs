421723b53a5b17a23943f1b21ca35547ab5d6c65
tejerolucas <tejerolucas@gmail.com>
2015-12-03 17:45:31 -0300

    Arreglo tab
    Agrego ripple tab

--- Assets/AnimatedTab.cs
+++ Assets/AnimatedTab.cs
@@ -6,13 +6,11 @@ public class AnimatedTab : MonoBehaviour {
 	public Image barra;
 	private Vector2 barrapos;
 	public Color col;
+	private LayoutElement[] layouts;
 
 	void Start(){
-		LayoutElement[] gos = this.transform.GetComponentsInChildren<LayoutElement> ();
-		foreach (LayoutElement lay in gos) {
-			//lay.flexibleWidth=-1;
-			Debug.Log("asd");
-		}
+		layouts = this.transform.GetComponentsInChildren<LayoutElement> ();
+		Invoke ("Flex", 0.3f);
 	}
 
 	public void Mover (GameObject go) {
@@ -23,6 +21,12 @@ public class AnimatedTab : MonoBehaviour {
 
 	void Update(){
 		barra.color = Color.Lerp(barra.color, col,0.05f);
+	}
+
+	void Flex(){
+		foreach(LayoutElement lay in layouts){
+			lay.flexibleWidth=1;
+		}
 	}
 	
 	// Update is called once per frame
