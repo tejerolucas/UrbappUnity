0409bbf58d3920b9711e40116a4eb2693cac5313
tejerolucas <tejerolucas@gmail.com>
2015-12-03 17:28:10 -0300

    Tabs
    Tabs con cambio de color
    Tool Anchors
    fotos noticias

---
+++ Assets/AnimatedTab.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnimatedTab : MonoBehaviour {
+	public Image barra;
+	private Vector2 barrapos;
+	public Color col;
+
+	void Start(){
+		LayoutElement[] gos = this.transform.GetComponentsInChildren<LayoutElement> ();
+		foreach (LayoutElement lay in gos) {
+			//lay.flexibleWidth=-1;
+			Debug.Log("asd");
+		}
+	}
+
+	public void Mover (GameObject go) {
+		barrapos = barra.rectTransform.anchoredPosition;
+		col = go.GetComponentInChildren<Text> ().color;
+		iTween.ValueTo (this.gameObject, iTween.Hash ("time",0.3f,"easetype",iTween.EaseType.easeInCubic,"to",go.GetComponent<RectTransform>().anchoredPosition.x,"from",barrapos.x,"onupdatetarget",this.gameObject,"onupdate","refresh"));
+	}
+
+	void Update(){
+		barra.color = Color.Lerp(barra.color, col,0.05f);
+	}
+	
+	// Update is called once per frame
+	void refresh (float valor) {
+		barra.rectTransform.anchoredPosition = new Vector2 (valor, barrapos.y);
+	}
+}
---
+++ Assets/colorfade.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class colorfade : MonoBehaviour {
+	public Image imagen;
+	public Color[] colores;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			imagen.color=colores[Random.Range(0,colores.Length)];
+		}
+	}
+}
